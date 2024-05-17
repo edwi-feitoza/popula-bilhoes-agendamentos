a14419e68274bc21f2a8b25b21ffffe57e21126d
Edwi Feitoza <edwi.slacker@gmail.com>
2024-05-17 20:33:41 -0300

    Initial commit

---
+++ scripts/popula_agendamentos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export DB_DATABASE="teste_bilhoes"
+export DB_HOST="mainserver.edwifeitoza.com.br"
+export DB_PASSWORD="meuovocaraio"
+export DB_USER="root"
+for dias_para_somar in {0..364}
+do
+    export DATA_EFETIVACAO=`date --date "+$dias_para_somar days" +'%Y-%m-%d %H:%M:%S'`
+    python3 ../main.py $DATA_EFETIVACAO > ~/bilhao.log 2>&1 &
+done
